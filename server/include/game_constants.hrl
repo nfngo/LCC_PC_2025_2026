732169d4a15ad30d7ff38b8cacb8840667bcade9
Nuno <nfnoliveira@hotmail.com>
2026-05-05 13:19:14 +0100

    Update game constants

--- server/include/game_constants.hrl
+++ server/include/game_constants.hrl
@@ -1,4 +1,9 @@
 %% File: game_constants.hrl
 
+% Dimensões do mapa
 -define(MAP_WIDTH, 1280).
 -define(MAP_HEIGHT, 720).
+% Taxa de atualização (50ms para 20 FPS)
+-define(TICK_INTERVAL, 50).
+% Duração do jogo em milissegundos (2 minutos)
+-define(GAME_TIME, 120000).
