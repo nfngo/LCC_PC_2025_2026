26c250f5169c6ae09321cc185a6a69cbea4de075
Nuno <nfnoliveira@hotmail.com>
2026-05-05 13:22:34 +0100

    Implement collision detection, handle player movement, and add an independent timer to control game time

--- server/game_session.erl
+++ server/game_session.erl
@@ -7,35 +7,40 @@ start_game(PlayerPids, MatchmakerPid) ->
     spawn(fun() -> init(PlayerPids, MatchmakerPid) end).
 
 init(PlayerPids, MatchmakerPid) ->
-    % Inicializa avatares dos jogadores
+    % Inicializar avatares dos jogadores
     Players = create_players(PlayerPids),
     io:format("GAME_SESSION: players:\n ~p~n", [Players]),
 
-    % Inicializa Foods com IDs de 1 a 12
+    % Inicializar Foods com IDs de 1 a 12
     Foods = maps:from_list([{I, create_food(I)} || I <- lists:seq(1, 12)]),
     io:format("GAME_SESSION: foods:\n ~p~n", [Foods]),
 
-    % Inicializa Poisons com IDs de 100 a 115 (para evitar colisão com IDs de Foods)
+    % Inicializar Poisons com IDs de 100 a 115 (para evitar colisão com IDs de Foods)
     Poisons = maps:from_list([{I, create_poison(I)} || I <- lists:seq(100, 115)]),
     io:format("GAME_SESSION: poisons:\n ~p~n", [Poisons]),
 
     % Notificar jogadores
     [Pid ! {game_started, self()} || Pid <- PlayerPids],
 
-    loop(Players, Foods, Poisons, MatchmakerPid).
+    % Enviar a mensagem 'tick' para este processo (self()) a cada 50ms
+    timer:send_interval(?TICK_INTERVAL, self(), tick),
 
-% Cria jogadores e atribui posições iniciais no mapa
+    loop(Players, Foods, Poisons, MatchmakerPid, ?GAME_TIME).
+
+% Criar jogadores e atribui posições iniciais no mapa
 create_players(PlayerPids) ->
     Positions = game_logic:calculate_spawn_positions(length(PlayerPids), 200),
 
+    Ids = lists:seq(1, length(PlayerPids)),
+
     maps:from_list(
         [
-            {Pid, #player{id = Pid, x = X, y = Y}}
-         || {Pid, {X, Y}} <- lists:zip(PlayerPids, Positions)
+            {Pid, #player{id = Id, x = X, y = Y}}
+         || {Pid, Id, {X, Y}} <- lists:zip3(PlayerPids, Ids, Positions)
         ]
     ).
 
-% Cria comida com raio entre 5 e 25 e massa entre 10 e 40
+% Criar comida com raio entre 5 e 25 e massa entre 10 e 40
 create_food(Id) ->
     #food{
         id = Id,
@@ -45,7 +50,7 @@ create_food(Id) ->
         mass = 9 + rand:uniform(31)
     }.
 
-% Cria veneno com raio entre 8 e 35 e massa entre 10 e 35
+% Criar veneno com raio entre 8 e 35 e massa entre 10 e 35
 create_poison(Id) ->
     #poison{
         id = Id,
@@ -55,32 +60,97 @@ create_poison(Id) ->
         mass = 9 + rand:uniform(26)
     }.
 
-loop(Players, Foods, Poisons, MatchmakerPid) ->
+loop(Players, Foods, Poisons, MatchmakerPid, TimeLeft) ->
     receive
-        {input, PlayerId, {Left, Up, Right}} ->
-            Player = maps:get(PlayerId, Players),
-            % Cria uma versão atualizada apenas com os novos estados
-            NewPlayer = Player#player{
-                moving_up = Up,
-                moving_left = Left,
-                moving_right = Right
-            },
-            % Guarda no mapa e continua o loop
-            NewPlayers = maps:put(PlayerId, NewPlayer, Players),
-            loop(NewPlayers, Foods, Poisons, MatchmakerPid);
+        {input, PlayerPid, {Left, Up, Right}} ->
+            case maps:find(PlayerPid, Players) of
+                {ok, Player} ->
+                    % Criar uma versão atualizada apenas com os novos estados
+                    NewPlayer = Player#player{
+                        moving_up = Up,
+                        moving_left = Left,
+                        moving_right = Right
+                    },
+                    % Guardar no mapa e continuar o loop
+                    NewPlayers = maps:put(PlayerPid, NewPlayer, Players),
+                    loop(NewPlayers, Foods, Poisons, MatchmakerPid, TimeLeft);
+                error ->
+                    loop(Players, Foods, Poisons, MatchmakerPid, TimeLeft)
+            end;
         tick ->
-            ok
-    after 120000 ->
-        % A implementar:
-        % Calcular e enviar resultados
-        Score = 100,
-        maps:foreach(
-            fun(Pid, _Player) ->
-                Pid ! {game_over, Score}
-            end,
-            Players
-        ),
-        io:format("GAME_SESSION: Game finished. Notifying matchmaker...~n"),
-        MatchmakerPid ! game_finished,
-        ok
+            % Calcular quanto tempo falta
+            NewTimeLeft = TimeLeft - ?TICK_INTERVAL,
+
+            if
+                % Se o tempo acabou, terminar o jogo
+                NewTimeLeft =< 0 ->
+                    end_game(Players, MatchmakerPid);
+                true ->
+                    % Processar o movimento de cada jogador
+                    MovedPlayers = process_movement(Players),
+
+                    % Verificar Colisões
+                    {FinalPlayers, FinalFoods, FinalPoisons} = check_collisions(
+                        MovedPlayers, Foods, Poisons
+                    ),
+
+                    % C. Enviar novo estado para todos os jogadores
+                    % broadcast_state(FinalPlayers, FinalFoods, FinalPoisons),
+
+                    loop(MovedPlayers, Foods, Poisons, MatchmakerPid, NewTimeLeft)
+            end
     end.
+
+process_movement(Players) ->
+    maps:map(
+        fun(_, P) ->
+            % Aplicar alterações baseadas no input recebido do jogador
+            P1 =
+                if
+                    P#player.moving_up -> game_logic:accelerate_forward(P);
+                    true -> P
+                end,
+            P2 =
+                if
+                    P#player.moving_left -> game_logic:turn_left(P1);
+                    true -> P1
+                end,
+            P3 =
+                if
+                    P#player.moving_right -> game_logic:turn_right(P2);
+                    true -> P2
+                end,
+
+            % Mover o objeto
+            game_logic:update_player(P3)
+        end,
+        Players
+    ).
+
+check_collisions(Players, Foods, Poisons) ->
+    % Verificar colisões com Comida
+    {UpdatedPlayers1, RemainingFoods} = game_logic:check_food_collisions(Players, Foods),
+
+    % Verificar colisões com Veneno
+    {UpdatedPlayers2, RemainingPoisons} = game_logic:check_poison_collisions(
+        UpdatedPlayers1, Poisons
+    ),
+
+    % Verificar colisões entre jogadores
+    {FinalPlayers} = game_logic:check_player_collisions(UpdatedPlayers2),
+
+    {FinalPlayers, RemainingFoods, RemainingPoisons}.
+
+end_game(Players, MatchmakerPid) ->
+    % A implementar:
+    % Calcular e enviar resultados
+    Score = 100,
+    maps:foreach(
+        fun(Pid, _) ->
+            Pid ! {game_over, Score}
+        end,
+        Players
+    ),
+    io:format("GAME_SESSION: Game finished. Notifying matchmaker...~n"),
+    MatchmakerPid ! game_finished,
+    ok.
