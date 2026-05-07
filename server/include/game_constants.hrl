ac34c182945e94045f30a108e0073c870cbaa387
Nuno <nfnoliveira@hotmail.com>
2026-05-07 10:01:53 +0100

    Update game constants

--- server/include/game_constants.hrl
+++ server/include/game_constants.hrl
@@ -7,3 +7,6 @@
 -define(TICK_INTERVAL, 50).
 % Duração do jogo em milissegundos (2 minutos)
 -define(GAME_TIME, 120000).
+% Número mínimo de Foods e Poisons no mapa
+-define(MIN_FOODS, 6).
+-define(MIN_POISONS, 8).
